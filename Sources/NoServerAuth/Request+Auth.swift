ed393b1196407f174c6e42c4947c855c0edb09c4
Guerson <ultim8p@gmail.com>
2024-09-28 13:16:47 -0500

    Add debug logs.

--- Sources/NoServerAuth/Request+Auth.swift
+++ Sources/NoServerAuth/Request+Auth.swift
@@ -15,27 +15,35 @@ public extension Request {
     func authenticateCredentials(in db: MongoDatabase, authClosure: ServerCredentialsAuthenticatedClosure?) async throws {
         let bearer = try bearerData
         let privateKey = try await db.appPrivateKey
-        try await authenticateCredentials(in: db,
-                                          privateKey: privateKey,
-                                          auth: bearer,
-                                          authClosure: authClosure)
+        print("OTP: NoServerAuth: bearer: \(bearer) PKey: \(privateKey)")
+        try await authenticateCredentials(
+            in: db,
+            privateKey: privateKey,
+            auth: bearer,
+            authClosure: authClosure
+        )
     }
     
-    func authenticateCredentials(in db: MongoDatabase,
-                                 privateKey: String?,
-                                 auth: Data?,
-                                 authClosure: ServerCredentialsAuthenticatedClosure?) async throws {
-        guard let privateKey = privateKey,
-              let credentials = auth
+    func authenticateCredentials(
+        in db: MongoDatabase,
+        privateKey: String?,
+        auth: Data?,
+        authClosure: ServerCredentialsAuthenticatedClosure?
+    ) async throws {
+        guard
+            let privateKey = privateKey,
+            let credentials = auth
         else { throw NoServerAuthError.missingCredentials }
         
         // 1 Decrypt Auth using privateKey
         let auth: AuthCredentials = try privateKey.aesDecrypt(data: credentials)
+        print("OTP: NoServerAuth: Id: \(auth._id) TOKEN: \(auth.token) ENTITY: \(auth.entity)")
         
         // 2 Find Credentials by Auth._id
         guard let authId = auth._id else { throw NoServerAuthError.missingCredentials }
+        print("OTP: NoServerAuth: finding auth id: \(authId)")
         let creds: ServerCredentials = try await ServerCredentials.findOne(in: db, id: authId)
-        
+        print("OTP:  NoServerAuth: found server credentuals for: \(authId): ID: \(creds._id) PKey: \(creds.privateKey) Entity: \(creds.entity) EntityId: \(creds.entityId) Device: \(creds.deviceName) AppId: \(creds.appIdentifier) OTP: Key: \(creds.otpKey)")
         // 3 Verify otp token by generating with local Credentials and matching to Auth object.
         try creds.verify(auth: auth)
         
@@ -44,7 +52,7 @@ public extension Request {
         
         // 5 Check if there is embedded Auth
         guard let nextAuth = auth.nextAuth else { return }
-        
+        print("OTP: NoServerAuth: CHECKING NEXT AUTH: \(auth.nextAuth)")
         // 6 Verify the next credentials by decripting with the current Credentials
         try await authenticateCredentials(
             in: db,
--- Sources/NoServerAuth/ServerCredentials+Verification.swift
+++ Sources/NoServerAuth/ServerCredentials+Verification.swift
@@ -16,21 +16,27 @@ public extension ServerCredentials {
         guard let privateKey = privateKey,
               let tokenData = auth.token
         else { throw NoServerAuthError.missingCredentials }
+        print("OTP: NoServerAuth: decrypting token: WithKey: \(privateKey)")
         return try privateKey.aesDecrypt(data: tokenData)
-    }
-    
-    func verify(token: AuthToken) throws {
-        guard let otpKey = otpKey,
-              let tokenValue = token.token
-        else { throw NoServerAuthError.missingCredentials }
-        
-        try otpKey.validateOTP(token: tokenValue,
-                               interval: AuthCredentialsDefault.otpInterval,
-                               range: AuthCredentialsDefault.otpRangeValidation)
     }
     
     func verify(auth: AuthCredentials) throws {
         let token = try tokenObject(from: auth)
+        print("OTP: NoServerAuth: decrypt token: \(token.token)")
         try verify(token: token)
+    }
+    
+    func verify(token: AuthToken) throws {
+        guard
+            let otpKey = otpKey,
+            let tokenValue = token.token
+        else { throw NoServerAuthError.missingCredentials }
+        print("OTP: NoServerAuth: validate token: \(tokenValue) OTP Key: \(otpKey)")
+        
+        try otpKey.validateOTP(
+            token: tokenValue,
+            interval: AuthCredentialsDefault.otpInterval,
+            range: AuthCredentialsDefault.otpRangeValidation
+        )
     }
 }
